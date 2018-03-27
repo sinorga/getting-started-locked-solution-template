177bd3c763ce005cddb8d09765a62e6f1549f0f0
Charlie G-N <charliegn@exosite.com>
2018-03-27 14:38:15 -0500

    Initial commit

---
+++ endpoints/healthcheck.get.lua
@@ -0,0 +1,28 @@
+--#ENDPOINT GET /healthcheck
+-- this will return a default json payload that passes healthcheck endpoint validation
+
+--[[
+Upon successful install of the application, this enpoint should return a payload that 
+matches the following schema:
+
+{
+	"Application":{
+		id:"...",
+		application_type: "rcm|hamv"
+		application_version: "1.3.1",
+		status:"online|offline|pending|degraded",
+		capabilities:[
+		   {"title":"User",
+       "description":"User profile and auth functionality",
+       "status":"online|offline|needs config",
+       [ "data":[{"Total Users":4494}, {...}] ]
+       }
+		]
+	}
+}
+
+--]]
+
+local JSONreply = {}
+JSONreply.status = "pending"
+return JSONreply
